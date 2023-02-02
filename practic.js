d4dedec32add05ae0a70a4489d3b3696f3640182
Діма Якименко <akimenkodima154@gmail.com>
2023-02-02 16:53:30 +0000

    first commit

---
+++ practic.js
@@ -0,0 +1,153 @@
+// задача номер 1.1
+// console.log(prompt("Введіть імя та призвище"))
+// console.log(prompt("Введіть адресу доставки"))
+// console.log(prompt("Введіть телефон"))
+
+// задача номер1.2
+// console.log(prompt("Введіть імя та призвище"))
+// console.log(prompt("Введіть назву навчального закладу"))
+
+// задача номер 1.3
+// console.log(prompt("Введіть імя та призвище"))
+// console.log(prompt("Введіть назву вашої професії"))
+// console.log(prompt("Введіть місце працювання"))
+
+// задача номер 1.4
+// console.log(prompt("Введіть імя та призвище"))
+// console.log(prompt("Введіть адресу проживання"))
+// console.log(prompt("Введіть телефон"))
+
+//////////////////////////////////////////////////////////
+
+//задача номер 2.1
+
+// const productName = 'Phone';
+// const pricePerItem = 15 000;
+// const isOnSale = true;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+//задача номер 2.2
+
+// const productName = 'Bike';
+// const pricePerItem = 10 000;
+// const isOnSale = false;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+//задача номер 2.3
+
+// const productName = 'Headphone';
+// const pricePerItem = 1 000;
+// const isOnSale = false;
+
+// console.log(productName);
+// console.log(pricePerItem);
+// console.log(isOnSale);
+
+//задача номер 2.4
+
+//////////////////////////////////////////////////////////
+
+//задача номер 3.1
+
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Headphone"
+// const productName = "Headphone";
+
+// ціна за штуку - число 1 000
+// const pricePerItem = 1000;
+
+// і вивести дані в консоль
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+//задача номер 3.2
+
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "Pizza"
+// const productName = "Headphone";
+
+// const pricePerItem = 1000;
+// ціна за штуку - число 300
+
+// і вивести дані в консоль
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+//задача номер 3.3
+
+// Оголоси дві змінні, productName для назви товару і pricePerItem для зберігання цни за штуку.
+// При оголошені присвой змінним наступні характеристики товара:
+
+// назва - строка "T-shirt"
+// const productName = "Headphone";
+
+// ціна за штуку - число 1 200;
+// const pricePerItem = 1000;
+
+// і вивести дані в консоль
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+////////////////////////////////////////////////////
+
+//задача номер 4.1
+
+// Ім'я товару змінили на "AirProds" і збільшили його ціну на 500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Headphone";
+// productName = "AirProds";
+
+// let pricePerItem = 1 000;
+// pricePerItem = 500;
+
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+//задача номер 4.2
+
+// Ім'я товару змінили на "Hot Dog" і зменшили його ціну на 50 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "Pizza";
+// productName = "Hot Dog";
+
+// let pricePerItem = 300;
+// pricePerItem = 50;
+
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+//задача номер 4.3
+
+// Ім'я товару змінили на "Sweater" і збільшили його ціну на 500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// let productName = "T-shirt";
+// productName = "Sweater";
+
+// let pricePerItem = 1 200;
+// pricePerItem = 500;
+
+// console.log('Назва', productName);
+// console.log("Ціна", pricePerItem);
+
+//////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
